65d6f5d627b493f72002c166582f9903d0e74c5c
Todd <todd.pressley1@gmail.com>
2014-03-06 22:42:08 -0500

    biggest commit ever

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -5,10 +5,10 @@ chrome.runtime.sendMessage({method: "getLocalStorage", key: "lists"}, function(r
 		var lists = response.data;
 		blacklist = lists.blacklist;
 		whitelist = lists.whitelist;
-		
+
 		console.log("blacklist", blacklist, "whitelist", whitelist);
 });	
 
 document.addEventListener('DOMNodeInserted', function(e) {
-	
+
 });
